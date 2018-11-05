4feadfc9c1208b0f504f79809c0a017f00853020
Dinn Eferet <dinn.eferet@gmail.com>
2018-11-05 08:56:43 +0100

    Add files via upload

--- open_inventory_desktop.sql
+++ open_inventory_desktop.sql
@@ -16,6 +16,57 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `a_items`
+--
+
+DROP TABLE IF EXISTS `a_items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `a_items` (
+  `item_name` varchar(33) NOT NULL,
+  `quantity` int(11) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  PRIMARY KEY (`item_name`),
+  UNIQUE KEY `item_name` (`item_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `a_items`
+--
+
+LOCK TABLES `a_items` WRITE;
+/*!40000 ALTER TABLE `a_items` DISABLE KEYS */;
+INSERT INTO `a_items` VALUES ('Cardboard',266,150);
+/*!40000 ALTER TABLE `a_items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `a_sales`
+--
+
+DROP TABLE IF EXISTS `a_sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `a_sales` (
+  `item_name` varchar(33) NOT NULL,
+  `quantity_bought` int(11) DEFAULT NULL,
+  `amount_paid` float DEFAULT NULL,
+  `date_of_sale` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `a_sales`
+--
+
+LOCK TABLES `a_sales` WRITE;
+/*!40000 ALTER TABLE `a_sales` DISABLE KEYS */;
+INSERT INTO `a_sales` VALUES ('Monster',23,52900,'2018-11-04'),('Cardboard',34,5100,'2018-11-04');
+/*!40000 ALTER TABLE `a_sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `eferet_items`
 --
 
@@ -37,7 +88,7 @@ CREATE TABLE `eferet_items` (
 
 LOCK TABLES `eferet_items` WRITE;
 /*!40000 ALTER TABLE `eferet_items` DISABLE KEYS */;
-INSERT INTO `eferet_items` VALUES ('5 Alive Large',155,350),('Faro Water',200,100),('Galaxy Chocolate Large',50,1000),('HP Notebook 15 PC',25,105000),('Indomie Carton',0,1900),('Monster',609,300),('Orbit Gum',466,350),('Pepsi Medium',360,120),('Snickers',300,300),('Twix',300,300);
+INSERT INTO `eferet_items` VALUES ('Faro Water',89,100),('Galaxy Chocolate Large',39,1000),('HB Pencil',280,25),('HP Notebook 15 PC',17,105000),('Indomie Carton',33,1900),('Monster',609,300),('Orbit Gum',463,350),('Pepsi Medium',314,120),('Snickers',232,300),('Twix',300,300);
 /*!40000 ALTER TABLE `eferet_items` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +113,7 @@ CREATE TABLE `eferet_sales` (
 
 LOCK TABLES `eferet_sales` WRITE;
 /*!40000 ALTER TABLE `eferet_sales` DISABLE KEYS */;
-INSERT INTO `eferet_sales` VALUES ('Monster',34,10200,'2018-10-31'),('Monster',23,6900,'2018-10-31'),('HP Notebook 15 PC',300,31500000,'2018-10-31'),('Pepsi Medium',40,4800,'2018-10-31'),('5 Alive Large',45,15750,'2018-10-31'),('Orbit Gum',34,11900,'2018-10-31'),('HP Notebook 15 PC',5,525000,'2018-10-31'),('Indomie Carton',50,95000,'2018-10-31');
+INSERT INTO `eferet_sales` VALUES ('Monster',34,10200,'2018-10-31'),('Monster',23,6900,'2018-10-31'),('HP Notebook 15 PC',300,31500000,'2018-10-31'),('Pepsi Medium',40,4800,'2018-10-31'),('5 Alive Large',45,15750,'2018-10-31'),('Orbit Gum',34,11900,'2018-10-31'),('HP Notebook 15 PC',5,525000,'2018-10-31'),('Indomie Carton',50,95000,'2018-10-31'),('5 Alive Large',3,1050,'2018-10-31'),('Faro Water',45,4500,'2018-11-03'),('HP Notebook 15 PC',3,315000,'2018-11-03'),('HB Pencil',20,500,'2018-11-04'),('Faro Water',2,200,'2018-11-04'),('Galaxy Chocolate Large',3,3000,'2018-11-04'),('Snickers',34,10200,'2018-11-04'),('Orbit Gum',3,1050,'2018-11-04'),('Pepsi Medium',23,2760,'2018-11-04'),('Faro Water',3,300,'2018-11-04'),('Galaxy Chocolate Large',3,3000,'2018-11-04'),('Snickers',34,10200,'2018-11-04'),('Faro Water',2,200,'2018-11-04'),('Faro Water',2,200,'2018-11-04'),('Faro Water',3,300,'2018-11-04'),('Faro Water',34,3400,'2018-11-04'),('Galaxy Chocolate Large',2,2000,'2018-11-04'),('Faro Water',3,300,'2018-11-04'),('HP Notebook 15 PC',2,210000,'2018-11-05'),('Faro Water',12,1200,'2018-11-05'),('Galaxy Chocolate Large',3,3000,'2018-11-05'),('Faro Water',3,300,'2018-11-05'),('HP Notebook 15 PC',3,315000,'2018-11-05'),('Faro Water',2,200,'2018-11-05'),('Pepsi Medium',23,2760,'2018-11-05');
 /*!40000 ALTER TABLE `eferet_sales` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,7 +139,7 @@ CREATE TABLE `user_accounts` (
 
 LOCK TABLES `user_accounts` WRITE;
 /*!40000 ALTER TABLE `user_accounts` DISABLE KEYS */;
-INSERT INTO `user_accounts` VALUES ('Eferet Tech','eferet','eferet','2018-10-31');
+INSERT INTO `user_accounts` VALUES ('Testing','a','a','2018-11-03'),('Eferet Technologies','eferet','eferet','2018-10-31');
 /*!40000 ALTER TABLE `user_accounts` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -101,4 +152,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-10-31 14:09:56
+-- Dump completed on 2018-11-05  8:55:21
