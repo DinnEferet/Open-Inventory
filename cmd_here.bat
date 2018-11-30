d0bbee5491e925b56c2074938ee8209211adcfbd
Dinn Eferet <dinn.eferet@gmail.com>
2018-11-30 14:38:44 +0100

    Add files via upload

--- cmd_here.bat
+++ cmd_here.bat
@@ -1,2 +1,6 @@
+::This file routes Command Prompt to wherever it is stored (in most Windows systems anyway). 
+::Makes using Python to run source code easier. Got it from an online forum a couple years
+::ago and have been using t ever since.
+
 @set path=C:\Windows\System32\cmd.exe;%path%
-@%comspec% 
+@%comspec%
